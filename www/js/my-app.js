c8ca41a633bff6d46182497070f69e00fb30b557
ZhangYanPeng <cn.yanpengzhang@qq.com>
2018-09-22 16:09:15 +0800

    Initial commit

---
+++ www/js/my-app.js
@@ -0,0 +1,57 @@
+// Initialize app
+var myApp = new Framework7();
+var num = 1;
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
+
+function confirm(){
+    myApp.alert("么么哒！","爱你哦");
+}
+
+function refuse(){
+    n = num;
+    while(n == num){
+        n = Math.floor(Math.random()*10) + 1;
+    }
+    num = n;
+    $$(".btn-refuse").attr('style',"display: none;");
+    $$("#btn"+num).attr('style',"display: ;");
+}
